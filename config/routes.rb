40cdcecfec641207130a8e0ab219039c4f46ef02
Lee Faus <leefaus@github.com>
2016-04-13 17:25:10 -0400

    adding logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
-  destroy_user_session DELETE /users/sign_out(.:format)
+  delete destroy_user_session,  /users/sign_out(.:format)
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root 'welcome#index'
 end

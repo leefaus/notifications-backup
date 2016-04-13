43c9665cc97d442c485beeac231bf6665ccef36b
Lee Faus <leefaus@github.com>
2016-04-13 17:46:26 -0400

    reset to template

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,12 +266,12 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   # Heroku Application: powerful-mountain-88762
-  config.omniauth :github, 'e9bcb3554d05b338d712', 'c63376ffac7167334589ad460a4649d768188ec1', {
+  config.omniauth :github, 'CLIENT_ID', 'APP_SECRET', {
       :scope => 'user:email, repo, gist, public_repo',
       :client_options => {
-        :site => 'https://ec2-52-90-93-65.compute-1.amazonaws.com/api/v3',
-        :authorize_url => 'https://ec2-52-90-93-65.compute-1.amazonaws.com/login/oauth/authorize',
-        :token_url => 'https://ec2-52-90-93-65.compute-1.amazonaws.com/login/oauth/access_token',
+        :site => 'https://github.YOURDOMAIN.com/api/v3',
+        :authorize_url => 'https://github.YOURDOMAIN.com/login/oauth/authorize',
+        :token_url => 'https://github.YOURDOMAIN.com/login/oauth/access_token',
       }
     }
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
